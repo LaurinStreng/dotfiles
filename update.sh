76664b49348dfe7ceeae0347eecbe8f1b18631c6
Laurin Streng <l.streng@finatix.de>
2024-11-21 17:19:15 +0100

    Add Brew Install

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if brew --version ; then
+    echo "Brew is Installed"
+else
+    echo "Installing Brew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Installing Updating Brew Packages
 echo "Executing Brew Bundle"
