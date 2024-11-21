087e1b0b057f9b78bfc8625b89cafca392bfafa8
Laurin Streng <l.streng@finatix.de>
2024-11-21 17:15:01 +0100

    Move brew install

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Copy zsh conf
-
-cp zsh/.zshrc $HOME/.zshrc
-echo "zshrc is copyed to home dir"
-
 # Installing Updating Brew Packages
 echo "Executing Brew Bundle"
 cd brew/ || exit
 brew bundle
+
+# Copy zsh conf
+
+cp zsh/.zshrc $HOME/.zshrc
+echo "zshrc is copyed to home dir"
